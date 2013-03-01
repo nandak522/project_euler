974d4d72e183d23f0a43996843d4e30c0c53b62a
Arun <parun@zeomega.com>
2013-03-01 20:03:07 +0530

    corrected the code

--- 48.py
+++ 48.py
@@ -6,9 +6,10 @@ def last_ten_digits(number):
     sum = 0
     for i in range(1,number):
         sum += pow(i,i)
-    return sum[-10:]
+    print sum
+    return str(sum)[-10:]
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     print "sum %s" %last_ten_digits(1000)
 
