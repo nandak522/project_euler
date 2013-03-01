6a28b216d59c0d28adecf3403313254c4258193c
Nanda Kishore <jnandakishore@zeomega.com>
2013-03-01 19:04:30 +0530

    Adding 6

---
+++ 6.py
@@ -0,0 +1,23 @@
+'''
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+'''
+
+MAX_INT = 100
+
+def compute_sum_numbers(n):
+    return n*(n+1)/2
+
+def compute_sum_of_squares(n):
+    return n*(n+1)*(2*n+1)/6
+
+print compute_sum_of_squares(MAX_INT)-(compute_sum_numbers(MAX_INT)**2)
