a2477b599b409d232338f0b4af42954d47e80626
Nanda Kishore <jnandakishore@zeomega.com>
2013-09-18 14:49:46 +0530

    Timing added

--- 1.py
+++ 1.py
@@ -5,10 +5,15 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
+from clock import Timer
+
 MAX_INT = 1000
 
 req = []
-for i in range(3, MAX_INT):
-    if (i % 3 == 0) or (i % 5 == 0):
-        req.append(i)
-print reduce(lambda x,y: x+y,  req)
+
+with Timer() as t:
+    for i in range(3, MAX_INT):
+        if (i % 3 == 0) or (i % 5 == 0):
+            req.append(i)
+print 'answer:', reduce(lambda x, y: x+y,  req)
+print 'Time taken:%.03f seconds' % t.interval
---
+++ clock.py
@@ -0,0 +1,19 @@
+'''
+Usage:
+with Timer() as t:
+    do_something
+
+print t.interval
+'''
+
+import time
+
+
+class Timer:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
