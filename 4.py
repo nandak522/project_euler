bddb223731959a6b3b25a0673f7e422539f8e71b
Nanda Kishore <jnandakishore@zeomega.com>
2013-09-18 16:34:16 +0530

    Corrected the solution

--- 4.py
+++ 4.py
@@ -5,27 +5,25 @@ the product of two 2-digit numbers is 9009 = 91 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 '''
 
+from clock import Timer
+
 
 def is_palindrome(number):
-    string = str(number)
-    total_length = len(string)
-    for index in range(total_length/2):
-        if string[index] != string[total_length-index-1]:
-            return False
-    return True
+    return str(number) == str(number)[::-1]
+
 
 def main():
-    all_palindromes = []
-    small_int = 111
+    small_int = 100
     big_int = 999
-    upper_bound = big_int
-    while big_int > small_int:
-        for i in range(big_int, small_int-1, -1):
-            product = big_int*i
-            if is_palindrome(product):
-                all_palindromes.append(product)
-        big_int -= 1
-    return max(all_palindromes)
+    highest_product = 0
+    for first_number in range(big_int, small_int-1, -1):
+        for second_number in range(first_number, small_int-1, -1):
+            product = first_number * second_number
+            if is_palindrome(product) and product > highest_product:
+                highest_product = product
+    return highest_product
 
 if __name__ == '__main__':
-    print main()
+    with Timer() as t:
+        print 'answer:', main()
+    print 'Time taken:%.3f seconds' % t.interval
